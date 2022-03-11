7cbb4acc07d8e2d31be4bbf092b0429b6cc75d10
Kamruzzaman <zaman7u@gmail.com>
2022-03-11 23:58:18 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,15 +17,6 @@ module.exports = {
             },
         },
     },
-    variants: {
-        backgroundColor: ['hover', 'focus', 'active', 'odd', 'dark'],
-        display: ['responsive', 'dark'],
-        textColor: ['focus-within', 'hover', 'active', 'dark'],
-        placeholderColor: ['focus', 'dark'],
-        borderColor: ['focus', 'hover', 'dark'],
-        divideColor: ['dark'],
-        boxShadow: ['focus'],
-    },
     plugins: [
         require('@tailwindcss/forms')
     ],
