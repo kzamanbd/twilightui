030e2ceae082f8d52c5b65de881ac38013bb9b20
Kamruzzaman <zamandotbd@gmail.com>
2022-07-16 15:18:18 +0600

    added new tailwind dashboard design

--- assets/js/init-alpine.js
+++ assets/js/init-alpine.js
@@ -1,4 +1,4 @@
-function data() {
+function initApp() {
     function getThemeFromLocalStorage() {
         // if user already changed the theme, use it
         if (window.localStorage.getItem('dark')) {
@@ -6,17 +6,24 @@ function data() {
         }
 
         // else return their preferences
-        return (
-            !!window.matchMedia &&
-            window.matchMedia('(prefers-color-scheme: dark)').matches
-        )
+        return !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
     }
 
     function setThemeToLocalStorage(value) {
         window.localStorage.setItem('dark', value)
     }
 
+    const testUser = {
+        name: 'John Doe',
+        email: 'john@example.com',
+        title: 'Software Engineer',
+        title2: 'Web dev',
+        status: 'Active',
+        role: 'Owner',
+    }
+
     return {
+        users: [...Array(10).keys()].map(() => testUser),
         isDarkMode: getThemeFromLocalStorage(),
         toggleTheme() {
             this.isDarkMode = !this.isDarkMode
