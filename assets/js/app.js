51426108c9d6110a397c13b8893757a93b44cf3d
Kamruzzaman <zamandotbd@gmail.com>
2022-07-20 10:31:59 +0600

    update sidebar menu accordion style

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,23 @@ const pSidebar = new PerfectScrollbar('#sidebar-menu', {
     minScrollbarLength: 20,
 })
 pSidebar.update()
+document.addEventListener('alpine:init', () => {
+    Alpine.store('accordion', {
+        tab: 0,
+    })
+    Alpine.data('accordion', idx => ({
+        init() {
+            this.idx = idx
+        },
+        idx: -1,
+        handleClick() {
+            this.$store.accordion.tab = this.$store.accordion.tab === this.idx ? 0 : this.idx
+        },
+        handleRotate() {
+            return this.$store.accordion.tab === this.idx ? 'rotate-180' : ''
+        },
+        handleToggle() {
+            return this.$store.accordion.tab === this.idx ? `max-height: ${this.$refs.tab.scrollHeight}px` : ''
+        },
+    }))
+})
