a8d34ce3f1cf4d1b0bfb91160e1c4f24e25bf3ea
Kamruzzaman <kzamanbn@gmail.com>
2023-03-08 23:49:15 +0600

    added `tw-elements` package

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,5 +46,6 @@ module.exports = {
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
         require('prettier-plugin-tailwindcss'),
+        require("tw-elements/dist/plugin")
     ],
 };
