e21af9eda95a4d80369aa5dbcbfca15cc4963f24
Kamruzzaman <kzamanbn@gmail.com>
2023-03-19 15:18:57 +0600

    added postcss-import

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     plugins: {
-        tailwindcss: {},
         autoprefixer: {},
+        'postcss-import': {},
+        tailwindcss: {},
         ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
     },
 };
