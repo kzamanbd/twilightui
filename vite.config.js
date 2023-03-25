30679aadb4b65e705f9f2e58a1e64f43cfa3084f
Kamruzzaman <kzamanbn@gmail.com>
2023-03-26 00:02:16 +0600

    added crm and login page

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,13 @@ module.exports = defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                crm: resolve(__dirname, 'crm.html'),
+                login: resolve(__dirname, 'login.html'),
             },
         },
+    },
+    server: {
+        port: 3000,
+        open: true,
     },
 });
