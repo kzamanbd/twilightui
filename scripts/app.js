ca13a94e5287b8f8717d84204a4e4f69a04b8226
Kamruzzaman <kzamanbn@gmail.com>
2023-04-04 19:51:59 +0600

    added `@alpinejs/focus` packages

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
 // alpine.js config
 import collapse from '@alpinejs/collapse';
+import focus from '@alpinejs/focus';
 import persist from '@alpinejs/persist';
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.plugin(collapse);
 Alpine.plugin(persist);
+Alpine.plugin(focus);
 
 import 'animate.css';
 // simplebar
