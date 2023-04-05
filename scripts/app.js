0b1464107f4d717167c1342f2ad685ebf2e99d9b
Kamruzzaman <kzamanbn@gmail.com>
2023-04-05 19:50:47 +0600

    fixed modal issue

--- scripts/app.js
+++ scripts/app.js
@@ -104,8 +104,10 @@ window.ResizeObserver = ResizeObserver;
     Alpine.data('modal', (initialOpenState = false) => ({
         open: initialOpenState,
 
-        toggle() {
+        toggle(event) {
             this.open = !this.open;
+            // if click esc key
+            if (event && event.keyCode === 27) this.open = false;
         },
     }));
 
@@ -472,103 +474,5 @@ document.addEventListener('DOMContentLoaded', function () {
             dismissDropdownMenu();
         }
         isShowDropMenu = false;
-    });
-
-    // Handler that uses various data-* attributes to trigger
-    // specific actions, miming bootstraps attributes
-    const triggers = Array.from(document.querySelectorAll('[data-toggle="collapse"]'));
-
-    const fnMap = {
-        show: 'add',
-        hide: 'remove',
-        toggle: 'toggle',
-    };
-    const collapse = (selector, cmd) => {
-        const targets = Array.from(document.querySelectorAll(selector));
-        targets.forEach(target => {
-            target.classList[fnMap[cmd]]('show');
-        });
-    };
-
-    window.addEventListener(
-        'click',
-        ev => {
-            const elm = ev.target;
-            if (triggers.includes(elm)) {
-                const selector = elm.getAttribute('data-target');
-                collapse(selector, 'toggle');
-            }
-        },
-        false,
-    );
-
-    /********* modal common js *********/
-    // openModal
-    var modalTrigger = document.querySelectorAll('[data-tw-toggle="modal"]');
-    var isModalShow = false;
-    Array.from(modalTrigger).forEach(function (item) {
-        item.addEventListener('click', function () {
-            var target = this.getAttribute('data-tw-target').substr(1);
-            var modalWindow = document.getElementById(target);
-
-            if (modalWindow.classList.contains('hidden')) {
-                modalWindow.classList.remove('hidden');
-                document.body.classList.add('overflow-hidden');
-            } else {
-                modalWindow.classList.add('hidden');
-                document.body.classList.remove('overflow-hidden');
-            }
-            isModalShow = false;
-
-            if (item.getAttribute('data-tw-backdrop') == 'static') {
-                isModalShow = true;
-            }
-        });
-    });
-
-    // closeButton
-    var closeButton = document.querySelectorAll('[data-tw-dismiss="modal"]');
-    Array.from(closeButton).forEach(function (subElem) {
-        subElem.addEventListener('click', function () {
-            var modalWindow = subElem.closest('.modal');
-            if (modalWindow.classList.contains('hidden')) {
-                modalWindow.classList.remove('hidden');
-                document.body.classList.add('overflow-hidden');
-            } else {
-                modalWindow.classList.add('hidden');
-                document.body.classList.remove('overflow-hidden');
-            }
-        });
-    });
-
-    // closeModal
-    var modalElem = document.querySelectorAll('.modal');
-    Array.from(modalElem).forEach(function (elem) {
-        // modalOverlay
-        var modalOverlay = elem.querySelectorAll('.modal-overlay');
-        Array.from(modalOverlay).forEach(function (subItem) {
-            subItem.addEventListener('click', function () {
-                if (!isModalShow) {
-                    if (elem.classList.contains('hidden')) {
-                        elem.classList.remove('hidden');
-                        document.body.classList.add('overflow-hidden');
-                    } else {
-                        elem.classList.add('hidden');
-                        document.body.classList.remove('overflow-hidden');
-                    }
-                }
-            });
-        });
-
-        // Escape
-        document.addEventListener('keydown', function (event) {
-            var key = event.key;
-            if (!isModalShow) {
-                if (key == 'Escape') {
-                    elem.classList.add('hidden');
-                    document.body.classList.remove('overflow-hidden');
-                }
-            }
-        });
     });
 })();
