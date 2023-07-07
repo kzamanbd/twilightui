a377cd7a43995e313d703597b8fd77e82f02d0db
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-07-07 15:14:03 +0600

    update colors

--- public/features/dashboard/hrm.init.js
+++ public/features/dashboard/hrm.init.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
             },
         },
         grid: {
-            borderColor: '#f2f6f7',
+            borderColor: 'var(--primary)',
         },
         responsive: [
             {
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 },
             },
         ],
-        colors: ['#5a66f1', '#60a5fa', '#d5dde7'],
+        colors: ['var(--primary)', '#60a5fa', '#d5dde7'],
         legend: {
             show: true,
             borderRadius: 5,
