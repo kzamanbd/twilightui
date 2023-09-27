7577c9c48e69395b04730c03f281f5f3339d886b
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-09-27 19:15:30 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,47 +8,48 @@ const aspectRatio = require('@tailwindcss/aspect-ratio');
 const { addDynamicIconSelectors } = require('@iconify/tailwind');
 
 const primary = colors.green;
+const colorConfig = {
+    dark: {
+        ...colors.slate,
+        DEFAULT: '#1F2937',
+    },
+    white: {
+        DEFAULT: '#FFFFFF',
+        light: '#E0E6ED',
+    },
+    light: '#F5F8FA',
+    primary: {
+        ...primary,
+        DEFAULT: '#00A76F',
+    },
+    info: {
+        ...colors.indigo,
+        DEFAULT: colors.indigo[500],
+    },
+    danger: {
+        ...colors.rose,
+        DEFAULT: colors.rose[500],
+    },
+    success: {
+        ...colors.emerald,
+        DEFAULT: colors.emerald[500],
+    },
+    warning: {
+        ...colors.amber,
+        DEFAULT: colors.amber[500],
+    },
+    secondary: {
+        ...colors.gray,
+        DEFAULT: colors.gray[400],
+    },
+};
 
 module.exports = {
     content: ['./src/**/*.{html,js}'],
     darkMode: 'class', // or 'media'
     theme: {
         extend: {
-            colors: {
-                dark: {
-                    ...colors.slate,
-                    DEFAULT: '#1F2937',
-                },
-                white: {
-                    DEFAULT: '#FFFFFF',
-                    light: '#E0E6ED',
-                },
-                light: '#F5F8FA',
-                primary: {
-                    ...primary,
-                    DEFAULT: '#00A76F',
-                },
-                info: {
-                    ...colors.indigo,
-                    DEFAULT: colors.indigo[500],
-                },
-                danger: {
-                    ...colors.rose,
-                    DEFAULT: colors.rose[500],
-                },
-                success: {
-                    ...colors.emerald,
-                    DEFAULT: colors.emerald[500],
-                },
-                warning: {
-                    ...colors.amber,
-                    DEFAULT: colors.amber[500],
-                },
-                secondary: {
-                    ...colors.gray,
-                    DEFAULT: colors.gray[400],
-                },
-            },
+            colors: colorConfig,
             fontFamily: {
                 sans: ['Inter, Helvetica, "sans-serif"', ...defaultTheme.fontFamily.sans],
             },
