ddeac63a2e26deb75e32240c2cabdeb880f91d53
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-10-16 22:31:30 +0600

    wip

--- public/features/widgets.js
+++ public/features/widgets.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    'use strict';
-
     // Bitcoin Chart
     var options = {
         series: [
@@ -433,227 +431,223 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // Target report chart
-    function targetReport() {
-        var options = {
-            series: [
-                {
-                    data: [17, 22, 37, 47, 39, 28, 14],
-                    name: 'Revenue',
-                },
-            ],
-            chart: {
-                type: 'bar',
-                height: 235,
-                toolbar: {
-                    show: false,
-                },
-                dropShadow: {
-                    enabled: true,
-                    enabledOnSeries: undefined,
-                    top: 6,
-                    left: 6,
-                    blur: 3,
-                    color: '#000',
-                    opacity: 0.05,
-                },
+    var options = {
+        series: [
+            {
+                data: [17, 22, 37, 47, 39, 28, 14],
+                name: 'Revenue',
             },
-            plotOptions: {
-                bar: {
-                    columnWidth: '35%',
-                    borderRadius: 4,
-                    horizontal: false,
-                    colors: {
-                        ranges: [
-                            {
-                                from: 41,
-                                to: Infinity,
-                                color: 'rgb(' + myVarVal + ')',
-                            },
-                            {
-                                from: 0,
-                                to: 40,
-                                color: 'rgba(' + myVarVal + ', 0.2)',
-                            },
-                        ],
-                    },
-                },
-            },
-            dataLabels: {
-                enabled: false,
-            },
-            grid: {
+        ],
+        chart: {
+            type: 'bar',
+            height: 235,
+            toolbar: {
                 show: false,
-                borderColor: 'transparent',
-                padding: {
-                    top: 0,
-                    right: 0,
-                    bottom: 0,
-                    left: 0,
-                },
-                yaxis: {
-                    lines: {
-                        show: false,
-                    },
+            },
+            dropShadow: {
+                enabled: true,
+                enabledOnSeries: undefined,
+                top: 6,
+                left: 6,
+                blur: 3,
+                color: '#000',
+                opacity: 0.05,
+            },
+        },
+        plotOptions: {
+            bar: {
+                columnWidth: '35%',
+                borderRadius: 4,
+                horizontal: false,
+                colors: {
+                    ranges: [
+                        {
+                            from: 41,
+                            to: Infinity,
+                            color: '#60a5fa',
+                        },
+                        {
+                            from: 0,
+                            to: 40,
+                            color: '#6366f1',
+                        },
+                    ],
                 },
             },
-            xaxis: {
-                categories: ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-                colors: '#fff',
-                axisBorder: {
-                    show: false,
-                },
-                axisTicks: {
-                    show: false,
-                },
-                labels: {
-                    rotate: -90,
-                    style: {
-                        fontFamily: 'Inter, sans-serif',
-                    },
-                },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        grid: {
+            show: false,
+            borderColor: 'transparent',
+            padding: {
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
             },
             yaxis: {
-                colors: '#fff',
-                axisBorder: {
-                    show: false,
-                },
-                axisTicks: {
-                    show: false,
-                },
-                labels: {
+                lines: {
                     show: false,
                 },
             },
-        };
-        document.querySelector('#report').innerHTML = '';
-        var chart = new ApexCharts(document.querySelector('#report'), options);
-        chart.render();
-    }
+        },
+        xaxis: {
+            categories: ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            colors: '#fff',
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false,
+            },
+            labels: {
+                rotate: -90,
+                style: {
+                    fontFamily: 'Inter, sans-serif',
+                },
+            },
+        },
+        yaxis: {
+            colors: '#fff',
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false,
+            },
+            labels: {
+                show: false,
+            },
+        },
+    };
+    document.querySelector('#report').innerHTML = '';
+    var chart = new ApexCharts(document.querySelector('#report'), options);
+    chart.render();
 
-    function pageviews() {
-        var options = {
-            series: [
-                {
-                    name: 'Job Applied',
-                    type: 'line',
-                    data: [25, 50, 30, 55, 20, 45, 30],
-                },
-                {
-                    name: 'Job Viewed',
-                    type: 'area',
-                    data: [35, 25, 40, 30, 45, 35, 60],
-                },
-            ],
-            chart: {
-                height: 348,
+    var options = {
+        series: [
+            {
+                name: 'Job Applied',
                 type: 'line',
-                stacked: false,
-                toolbar: {
-                    show: false,
-                },
-                dropShadow: {
-                    enabled: true,
-                    enabledOnSeries: undefined,
-                    top: 7,
-                    left: 1,
-                    blur: 3,
-                    color: '#000',
-                    opacity: 0.2,
-                },
+                data: [25, 50, 30, 55, 20, 45, 30],
             },
-            colors: ['rgba(' + myVarVal + ', 1)', 'rgba(' + myVarVal + ', 0.1)'],
-            grid: {
-                borderColor: '#e9edf4',
-                padding: {
-                    top: 10,
-                    right: 0,
-                    bottom: 0,
-                    left: 0,
-                },
+            {
+                name: 'Job Viewed',
+                type: 'area',
+                data: [35, 25, 40, 30, 45, 35, 60],
             },
-            stroke: {
-                width: [2, 2],
-                curve: 'smooth',
-                dashArray: [0, 4],
+        ],
+        chart: {
+            height: 348,
+            type: 'line',
+            stacked: false,
+            toolbar: {
+                show: false,
             },
-            labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+            dropShadow: {
+                enabled: true,
+                enabledOnSeries: undefined,
+                top: 7,
+                left: 1,
+                blur: 3,
+                color: '#000',
+                opacity: 0.2,
+            },
+        },
+        colors: ['#60a5fa', '#6366f1'],
+        grid: {
+            borderColor: '#e9edf4',
+            padding: {
+                top: 10,
+                right: 0,
+                bottom: 0,
+                left: 0,
+            },
+        },
+        stroke: {
+            width: [2, 2],
+            curve: 'smooth',
+            dashArray: [0, 4],
+        },
+        labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        markers: {
+            size: 4,
+            hover: {
+                size: 5,
+            },
+        },
+        fill: {
+            opacity: [0.85, 0.25, 1],
+            gradient: {
+                inverseColors: false,
+                shadeIntensity: 1,
+                shade: 'light',
+                type: 'vertical',
+                opacityFrom: 0.85,
+                opacityTo: 0.55,
+                stops: [0, 100, 100, 100],
+            },
+        },
+        legend: {
+            show: true,
+            position: 'top',
+            fontFamily: 'Inter, sans-serif',
             markers: {
-                size: 4,
-                hover: {
-                    size: 5,
-                },
+                width: 10,
+                height: 10,
             },
-            fill: {
-                opacity: [0.85, 0.25, 1],
-                gradient: {
-                    inverseColors: false,
-                    shadeIntensity: 1,
-                    shade: 'light',
-                    type: 'vertical',
-                    opacityFrom: 0.85,
-                    opacityTo: 0.55,
-                    stops: [0, 100, 100, 100],
-                },
-            },
-            legend: {
+        },
+        xaxis: {
+            type: 'week',
+            axisBorder: {
                 show: true,
-                position: 'top',
-                fontFamily: 'Inter, sans-serif',
-                markers: {
-                    width: 10,
-                    height: 10,
+                color: 'rgba(119, 119, 142, 0.05)',
+                offsetX: 0,
+                offsetY: 0,
+            },
+            axisTicks: {
+                show: true,
+                borderType: 'solid',
+                color: 'rgba(119, 119, 142, 0.05)',
+                width: 6,
+                offsetX: 0,
+                offsetY: 0,
+            },
+            labels: {
+                rotate: -90,
+            },
+        },
+        yaxis: {
+            title: {
+                style: {
+                    color: '#adb5be',
+                    fontSize: '14px',
+                    fontFamily: 'Inter, sans-serif',
+                    fontWeight: 600,
                 },
             },
-            xaxis: {
-                type: 'week',
-                axisBorder: {
-                    show: true,
-                    color: 'rgba(119, 119, 142, 0.05)',
-                    offsetX: 0,
-                    offsetY: 0,
-                },
-                axisTicks: {
-                    show: true,
-                    borderType: 'solid',
-                    color: 'rgba(119, 119, 142, 0.05)',
-                    width: 6,
-                    offsetX: 0,
-                    offsetY: 0,
-                },
-                labels: {
-                    rotate: -90,
+            labels: {
+                formatter: function (y) {
+                    return y.toFixed(0) + '';
                 },
             },
-            yaxis: {
-                title: {
-                    style: {
-                        color: '#adb5be',
-                        fontSize: '14px',
-                        fontFamily: 'Inter, sans-serif',
-                        fontWeight: 600,
-                    },
-                },
-                labels: {
-                    formatter: function (y) {
-                        return y.toFixed(0) + '';
-                    },
+        },
+        tooltip: {
+            shared: true,
+            intersect: false,
+            y: {
+                formatter: function (y) {
+                    if (typeof y !== 'undefined') {
+                        return y.toFixed(0) + ' points';
+                    }
+                    return y;
                 },
             },
-            tooltip: {
-                shared: true,
-                intersect: false,
-                y: {
-                    formatter: function (y) {
-                        if (typeof y !== 'undefined') {
-                            return y.toFixed(0) + ' points';
-                        }
-                        return y;
-                    },
-                },
-            },
-        };
-        document.querySelector('#views').innerHTML = '';
-        var chart = new ApexCharts(document.querySelector('#views'), options);
-        chart.render();
-    }
+        },
+    };
+    document.querySelector('#views').innerHTML = '';
+    var chart = new ApexCharts(document.querySelector('#views'), options);
+    chart.render();
 });
